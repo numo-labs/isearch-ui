4317b5948d28a7fc1844fb378d3ecbfb32507e03
Nikhila Ravi <nikkiravi92@gmail.com>
2016-03-04 18:28:18 +0000

    fixes merge error

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
 import configureStore from './store/configure-store.js';
 import Root from './containers/root/index.js';
 
