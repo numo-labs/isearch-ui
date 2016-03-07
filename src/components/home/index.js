3e861463d28c22a201a332e2aff479803aa657cc
Nikhila Ravi <nikkiravi92@gmail.com>
2016-03-07 17:02:55 +0000

    Fixes styling of tiles

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -11,13 +11,34 @@ const masonryOptions = {
 
 class Gallery extends Component {
   render () {
+
+    const filter1 = {
+      part1: 'HOW IMPORTANT IS ',
+      bigWord: 'NIGHTLIFE ?',
+      part2:''
+    };
+
+    const filter2 = {
+      part1: 'SHOW ME PLACES WHERE ',
+      bigWord: 'FOOD & DRINK',
+      part2: 'GETS 4 STARS'
+    };
+
+    const filter3 = {
+      part1: 'SHOW ME PLACES WHERE ',
+      bigWord: 'SIGHTS',
+      part2: 'GETS 2 STARS'
+    };
+
     return (
       <Masonry
         elementType={'div'}
         options={masonryOptions}
         disableImagesLoaded={false}
       >
-        <FilterTile />
+        <FilterTile description={filter1} color={'#B9CAA8'}/>
+        <FilterTile description={filter2} color={'#F19024'}/>
+        <FilterTile description={filter3} color={'#DA3A68'}/>
         <SearchTile height='1' title='tile' />
       </Masonry>
     );
