85be3929b58119f5bea8bcd7d26d0a920ed74877
Nikhila Ravi <nikkiravi92@gmail.com>
2016-03-07 16:34:39 +0000

    Adds filter tile component and fonts

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import Masonry from 'react-masonry-component';
 import SearchTile from 'search-tile';
+import FilterTile from 'filter-tile';
 
 const masonryOptions = {
-  transitionDuration: 0
+  transitionDuration: 0,
+  columnWidth: 375,
+  gutter: 10 // horizontal spacing between tiles
 };
 
 class Gallery extends Component {
@@ -14,13 +17,7 @@ class Gallery extends Component {
         options={masonryOptions}
         disableImagesLoaded={false}
       >
-        <SearchTile height='1' title='tile' />
-        <SearchTile height='3' title='tile' />
-        <SearchTile height='2' title='tile' />
-        <SearchTile height='1' title='tile' />
-        <SearchTile height='1' title='tile' />
-        <SearchTile height='3' title='tile' />
-        <SearchTile height='2' title='tile' />
+        <FilterTile />
         <SearchTile height='1' title='tile' />
       </Masonry>
     );
--- webpack.config.js
+++ webpack.config.js
@@ -27,17 +27,21 @@ module.exports = {
         loader: 'file?name=[name].[ext]'
       },
       {
-        test: /\.css$/,
-        loader: 'style-loader!css-loader'
-      },
-      {
         test: /masonry-layout/,
         loader: 'imports?define=>false&this=>window'
       },
       {
         test: /imagesloaded/,
         loader: 'imports?define=>false&this=>window'
-      }
+      },
+      {
+        test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+        loader: 'file-loader?name=fonts/[name].[ext]'
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      },
     ]
   }
 };
