b7235dd378cdf921b5c3585b3522c788756e906d
Nikhila Ravi <nikkiravi92@gmail.com>
2016-03-08 16:17:57 +0000

    Updates webpack production config
    file-loader for the font files emits the file into the output directory..not srure if this necessary

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -34,7 +34,7 @@ module.exports = {
       },
       {
         test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
-        loader: 'file-loader'
+        loader: 'file-loader?name=[path][name].[ext]'
       },
       {
         test: /\.css$/,
