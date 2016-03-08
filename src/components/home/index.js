b04c56ee79c9076adb095c5e0ea1b3515932e3a8
Nikhila Ravi <nikkiravi92@gmail.com>
2016-03-08 14:08:01 +0000

    Changes package component to take props

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -14,27 +14,40 @@ const masonryOptions = {
 class Gallery extends Component {
   render () {
     const filter1 = {
-      part1: 'HOW IMPORTANT IS ',
-      bigWord: 'NIGHTLIFE ?',
+      part1: 'How important is ',
+      bigWord: 'nightlife ?',
       part2: ''
     };
 
     const filter2 = {
-      part1: 'SHOW ME PLACES WHERE ',
-      bigWord: 'FOOD & DRINK',
-      part2: 'GETS 4 STARS'
+      part1: 'Show me places where ',
+      bigWord: 'Food & Drink',
+      part2: 'gets 4 stars'
     };
 
     const filter3 = {
-      part1: 'SHOW ME PLACES WHERE ',
-      bigWord: 'SIGHTS',
-      part2: 'GETS 2 STARS'
+      part1: 'Show me places where ',
+      bigWord: 'sights',
+      part2: 'gets 2 stars'
     };
 
     const filter4 = {
-      part1: 'HOW MUCH ',
-      bigWord: 'SPORT & ADVENTURE',
-      part2: 'DO YOU WANT?'
+      part1: 'How much ',
+      bigWord: 'Sport & Adventure',
+      part2: 'do you want?'
+    };
+
+    const packageInfo = {
+      imageSrc: 'https://lh5.ggpht.com/X6S-3G1irwe649hOPWe2GYqcl4eSxK69r6Y1CGoC9oXk45MRwTTywpz0vdVoQS0_vVVX=h900',
+      caption: 'Child friendly',
+      title: 'Sunwing Side East Beach',
+      subtitle: 'Side, Antalya-area Turkey',
+      numHearts: 4,
+      rating: '4.4/5',
+      packageDetails: 'Breakfast buffet',
+      oldPrice: 30.280,
+      period: '16 nov 2015, 1 week' ,
+      newPrice: 27.440,
     };
 
     return (
@@ -49,7 +62,7 @@ class Gallery extends Component {
           <FilterTile description={filter2} color={'#F19024'}/>
           <FilterTile description={filter3} color={'#DA3A68'}/>
           <FilterTile description={filter4} color={'#8FB8C3'}/>
-          <PackageTile />
+          <PackageTile { ...packageInfo } />
           <SearchTile backgroundImage='https://igcdn-photos-b-a.akamaihd.net/hphotos-ak-xta1/t51.2885-15/e15/11245924_1592722294278649_2115388840_n.jpg' height='1' title='tile' />
           <SearchTile backgroundImage='https://igcdn-photos-b-a.akamaihd.net/hphotos-ak-xap1/t51.12885-15/e35/10354524_1569704863350745_1720947713_n.jpg' height='3' title='tile' />
           <SearchTile backgroundImage='https://igcdn-photos-b-a.akamaihd.net/hphotos-ak-xtp1/t51.12885-15/s750x750/sh0=108/e35/12479569_1511440029160330_2051512338_n.jpg' height='2' title='tile' />
