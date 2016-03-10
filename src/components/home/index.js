7f7bd63144de81ace30d498c61eb70a5974701a2
Nikhila Ravi <nikkiravi92@gmail.com>
2016-03-10 17:41:06 +0000

    Adds floating filter icon and modal to show tags

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -5,6 +5,7 @@ import SearchBar from 'search-bar';
 import * as mockData from './mockData.js';
 import Grid from './grid.js';
 import AddMessage from 'add-message';
+import Modal from './modal.js';
 
 require('./style.css');
 require('./normalise.css');
@@ -12,10 +13,13 @@ class Gallery extends Component {
   constructor () {
     super();
     this.state = {
-      addMessageVisible: false
+      addMessageVisible: false,
+      modalVisible: false
     };
     this.hideAddMessage = this.hideAddMessage.bind(this);
     this.showAddMessage = this.showAddMessage.bind(this);
+    this.showModal = this.showModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
   }
 
   hideAddMessage () {
@@ -28,16 +32,28 @@ class Gallery extends Component {
     }, 500);
   }
 
+  showModal () {
+    this.setState({modalVisible: true});
+  }
+
+  closeModal () {
+    this.setState({modalVisible: false});
+  }
+
   render () {
     const { searchSummary, ...tileData } = mockData;
     const { addMessageVisible } = this.state;
     return (
       <div className='homeContainer'>
+        <Modal showModal={this.state.modalVisible} close={this.closeModal}/>
         <SearchBar />
         <SearchSummary {...searchSummary} />
         <Tags />
         <Grid showAddMessage={this.showAddMessage} tileData={tileData}/>
         {addMessageVisible && <AddMessage hideAddMessage={this.hideAddMessage} suggestedLocations='Croatia and Greece'/>}
+        <div className='filterIcon' onClick={this.showModal}>
+          <i className='fa fa-filter fa-2x'/>
+        </div>
       </div>
     );
   }
---
+++ src/components/home/modal.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { Modal, Button } from 'react-bootstrap';
+
+class TagModal extends Component {
+  render() {
+    return (
+      <Modal show={this.props.showModal} onHide={this.props.close} dialogClassName='modal-xl'>
+        <Modal.Body>
+          <h4>TAGS WILL BE SHOWN HERE</h4>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={this.props.close}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
+}
+
+export default TagModal;
