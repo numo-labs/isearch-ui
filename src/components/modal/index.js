a167e9e130eca9535ce0c1e68c246e6d231c3e4c
Jack Carlisle <jackcarlisle@hotmail.co.uk>
2016-03-16 17:16:49 +0000

    adds tags to modal

--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -1,11 +1,13 @@
 import React, { Component, PropTypes } from 'react';
 import { Modal, Button } from 'react-bootstrap';
+import Tags from '../../containers/tags';
 
 class TagModal extends Component {
   render () {
     return (
       <Modal show={this.props.modalVisible} onHide={this.props.close} dialogClassName='modal-xl'>
         <Modal.Body>
+        <Tags />
           <h4>TAGS WILL BE SHOWN HERE</h4>
         </Modal.Body>
         <Modal.Footer>
