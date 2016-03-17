70775f8a4bdbd8a55aa30d4effc7a7a20ddfdc16
Jack Carlisle <jackcarlisle@hotmail.co.uk>
2016-03-17 17:14:14 +0000

    adds thunk middleware

--- src/store/configure-store.js
+++ src/store/configure-store.js
@@ -1,6 +1,11 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
 import rootReducer from '../reducers';
 
+const createStoreWithMiddleware = applyMiddleware(
+  thunkMiddleware
+)(createStore);
+
 export default function configureStore (initialState) {
-  return createStore(rootReducer);
+  return createStoreWithMiddleware(rootReducer, initialState);
 }
