d5df36f4cca1d1b98c2420a014cfa1d119ba268d
Jack Carlisle <jackcarlisle@hotmail.co.uk>
2016-03-17 17:42:33 +0000

    adds thunk example as well as redux logger

--- src/store/configure-store.js
+++ src/store/configure-store.js
@@ -1,9 +1,12 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import rootReducer from '../reducers';
+import createLogger from 'redux-logger';
 
+const logger = createLogger();
 const createStoreWithMiddleware = applyMiddleware(
-  thunkMiddleware
+  thunkMiddleware,
+  logger
 )(createStore);
 
 export default function configureStore (initialState) {
