e79ceaad45def825967c7ef277a8280f516b24d2
Jimmy Rutsfrom:(noreply@github.com)Rutsfrom:(noreply@github.com) <jimmy.ruts@thomascookonline.com>
2016-03-21 22:46:54 +0000

    put back webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 'use strict';
 var path = require('path');
 var webpack = require('webpack');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -41,11 +40,14 @@ module.exports = {
         test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
         loader: 'file-loader?name=fonts/[name].[ext]'
       },
-      { test: /\.css$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader") }
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      }
     ]
   },
   devServer: {hot: true},
-  plugins: [new webpack.HotModuleReplacementPlugin(), new ExtractTextPlugin('common-[hash:6].css', { allChunks: true })],
+  plugins: [new webpack.HotModuleReplacementPlugin()],
   inline: true,
   progress: true,
   colors: true
