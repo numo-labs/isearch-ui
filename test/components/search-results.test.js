b0905008c30cc75207141e492d9b71d43540d085
Jack Carlisle <jackcarlisle@hotmail.co.uk>
2016-04-08 14:12:35 +0100

    adds tests for the search results component

---
+++ test/components/search-results.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { expect } from 'chai';
+import SearchResults from '../../src/components/search-results';
+import mockTiles from '../../src/utils/mock-search-results';
+
+describe('Component', function () {
+  const wrapper = shallow(<SearchResults items={mockTiles}/>);
+  const children = wrapper.children().nodes;
+  describe('<SearchResults />', function () {
+    it('should render our SearchResults component', function (done) {
+      expect(children).to.have.length(3);
+      done();
+    });
+    it.only('should render 3 elements with the class .gridItem', function (done) {
+      expect(wrapper.find('.gridItem').length).to.equal(3);
+      done();
+    });
+  });
+});
