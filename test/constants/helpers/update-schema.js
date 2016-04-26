d1e7ee8e27c7431661e2452d30954f5723d507c7
Nikhila Ravi <nikkiravi92@gmail.com>
2016-04-26 09:55:17 +0100

    Updates npm packages to include graphql

--- test/constants/helpers/update-schema.js
+++ test/constants/helpers/update-schema.js
@@ -1,4 +1,4 @@
-import { introspectionQuery } from './query.js';
+import { introspectionQuery } from './introspectionQuery.js';
 import request from 'request';
 const graphqlApiURL = 'https://f0uih51vu0.execute-api.eu-west-1.amazonaws.com/ci/graphql';
 request({
@@ -12,7 +12,6 @@ request({
 }, function (err, res, body) {
   if (err) return console.error(err);
   if (body && body.errors) {
-    process.stderr.write(JSON.stringify(introspectionQuery));
     process.stderr.write(JSON.stringify(body.errors, null, 2));
     process.exit(1);
   } else if (body) {
