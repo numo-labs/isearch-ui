f50f2181198392ae972b6af8a88a91fa3299065e
Jack Carlisle <jackcarlisle@hotmail.co.uk>
2016-05-03 11:58:50 +0100

    removes passenger info from the helpers tests

--- test/actions/helpers.test.js
+++ test/actions/helpers.test.js
@@ -9,19 +9,20 @@ describe('Helpers', () => {
     {id: 'amenity:wifi', displayName: 'wifi'},
     {id: 'amenity:pool', displayName: 'pool'}
   ];
-  it('formatQuery: returns an object with passenger, geography and amenity keys', (done) => {
+  it('formatQuery: returns an object with geography and amenity keys', (done) => {
     const res = formatQuery(tags);
-    expect(Object.keys(res)).to.deep.equal(['passengers', 'geography', 'amenity']);
+    console.log(Object.keys(res));
+    expect(Object.keys(res)).to.deep.equal(['geography', 'amenity']);
     expect(res.geography).to.deep.equal(['geo:geonames:12345']);
     expect(res.amenity).to.deep.equal(['amenity:wifi', 'amenity:pool']);
     done();
   });
-  it('formatQuery: returns an object with passenger key and only geography key if there are no amenity tags', (done) => {
+  it('formatQuery: returns an object with geography key if there are no amenity tags', (done) => {
     const tags = [
       {id: 'geo:geonames:12345', displayName: 'spain'}
     ];
     const res = formatQuery(tags);
-    expect(Object.keys(res)).to.deep.equal(['passengers', 'geography']);
+    expect(Object.keys(res)).to.deep.equal(['geography']);
     expect(res.geography).to.deep.equal(['geo:geonames:12345']);
     done();
   });
