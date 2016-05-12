63ed059d915f0f817de097b673368bc97b5a8a9f
Jimmy Rutsfrom:(noreply@github.com)Rutsfrom:(noreply@github.com) <jimmy.ruts@thomascookonline.com>
2016-05-12 17:29:52 +0100

    Semistandaaaard

--- test/actions/search-results.test.js
+++ test/actions/search-results.test.js
@@ -3,7 +3,7 @@ import {
   SAVE_SEARCH_RESULT_ID,
   RECEIVE_SEARCH_RESULT,
   // TILES_ADD_TILES,
-  SEARCH_ERROR
+  //SEARCH_ERROR
 } from '../../src/constants/actionTypes';
 
 import { MUTATION_START_SEARCH } from '../../src/constants/mutations.js';
