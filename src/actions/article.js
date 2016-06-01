0640a190467111c145933510e40b2130a62999f9
Marcelo Gomez <gomezconesa.marcelo@gmail.com>
2016-06-01 15:49:58 +0200

    removed promises

--- src/actions/article.js
+++ src/actions/article.js
@@ -9,14 +9,13 @@ export const viewArticle = (content) => { return { type: VIEW_ARTICLE, content }
 
 export const getArticle = (bucketId, itemId) => {
   return (dispatch) => {
-    return new Promise((resolve, reject) => {
-      // JQuery imported at index.html and intex.template.html
-      $.getJSON(
-        `https://numo-search-results.s3.amazonaws.com/ci/${bucketId}/${itemId}.json`,
-        // 'https://s3-eu-west-1.amazonaws.com/numo-search-results/ci/TESTUSERID/testhotel/article.json',
-        resolve
-      );
-    }).then((data) => { console.info(data); dispatch(viewArticle(data.tile)); })
-      .catch((err) => console.error('ERROR : ', err));
+    // JQuery imported at index.html and intex.template.html
+    $.getJSON(
+      `https://numo-search-results.s3.amazonaws.com/ci/${bucketId}/${itemId}.json`,
+      (data) => {
+        console.info(data);
+        dispatch(viewArticle(data.tile));
+      }
+    );
   };
 };
--- src/actions/hotel.js
+++ src/actions/hotel.js
@@ -6,14 +6,13 @@ export const setHotelPage = (hotel) => { return {type: SET_HOTEL_PAGE, hotel}; }
 
 export const getHotel = (bucketId, itemId) => {
   return (dispatch) => {
-    return new Promise((resolve, reject) => {
-      // JQuery imported at index.html and intex.template.html
-      $.getJSON(
-        `https://numo-search-results.s3.amazonaws.com/ci/${bucketId}/${itemId}.json`,
-        // 'https://s3-eu-west-1.amazonaws.com/numo-search-results/ci/TESTUSERID/testhotel/hotel.json',
-        resolve
-      );
-    }).then((data) => { console.info(data); dispatch(setHotelPage(data.packageOffer)); })
-      .catch((err) => console.error('ERROR : ', err));
+    // JQuery imported at index.html and intex.template.html
+    $.getJSON(
+      `https://numo-search-results.s3.amazonaws.com/ci/${bucketId}/${itemId}.json`,
+      (data) => {
+        console.info(data);
+        dispatch(setHotelPage(data.packageOffer));
+      }
+    );
   };
 };
