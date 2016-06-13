97a2368b90522501d8432235aa28b6e1fb97ab2f
Tom Gallacher <tomgallacher23@gmail.com>
2016-06-13 11:46:57 +0100

    Updating to production url

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ const config = {
     graphqlUrl: 'https://f0uih51vu0.execute-api.eu-west-1.amazonaws.com/ci/graphql'
   },
   production: {
-    socketUrl: 'https://ci-socket-server.tcdl.io?auto_room=false',
+    socketUrl: 'https://socket-server.tcdl.io?auto_room=false',
     graphqlUrl: 'https://f0uih51vu0.execute-api.eu-west-1.amazonaws.com/prod/graphql'
   }
 };
