4d68ca6b8581108d1b5ed01a3177ca92b99f0c3c
Tom Gallacher <tomgallacher23@gmail.com>
2016-06-13 12:19:32 +0100

    Fixing bucketUrl

--- config.js
+++ config.js
@@ -1,15 +1,18 @@
 const config = {
   common: {
     socketUrl: 'https://ci-socket-server.tcdl.io?auto_room=false',
-    graphqlUrl: 'https://f0uih51vu0.execute-api.eu-west-1.amazonaws.com/ci/graphql'
+    graphqlUrl: 'https://f0uih51vu0.execute-api.eu-west-1.amazonaws.com/ci/graphql',
+    bucketUrl: 'https://numo-search-results.s3.amazonaws.com/ci/'
   },
   ci: {
     socketUrl: 'https://ci-socket-server.tcdl.io?auto_room=false',
-    graphqlUrl: 'https://f0uih51vu0.execute-api.eu-west-1.amazonaws.com/ci/graphql'
+    graphqlUrl: 'https://f0uih51vu0.execute-api.eu-west-1.amazonaws.com/ci/graphql',
+    bucketUrl: 'https://numo-search-results.s3.amazonaws.com/ci/'
   },
   production: {
     socketUrl: 'https://socket-server.tcdl.io?auto_room=false',
-    graphqlUrl: 'https://f0uih51vu0.execute-api.eu-west-1.amazonaws.com/prod/graphql'
+    graphqlUrl: 'https://f0uih51vu0.execute-api.eu-west-1.amazonaws.com/prod/graphql',
+    bucketUrl: 'https://numo-search-results.s3.amazonaws.com/prod/'
   }
 };
 
--- src/actions/hotel.js
+++ src/actions/hotel.js
@@ -1,4 +1,8 @@
 import { VIEW_HOTEL, SET_HOTEL_PAGE } from '../constants/actionTypes';
+import configuration from '../../config';
+import configure from 'con.figure';
+
+const config = configure(configuration);
 
 export const viewHotel = () => { return {type: VIEW_HOTEL}; };
 
@@ -8,7 +12,7 @@ export const getHotel = (bucketId, itemId) => {
   return (dispatch) => {
     // JQuery imported at index.html and intex.template.html
     $.getJSON(
-      `https://numo-search-results.s3.amazonaws.com/ci/${bucketId}/${itemId}.json`,
+      `${config.bucketUrl}${bucketId}/${itemId}.json`,
       (data) => {
         console.info(data);
         dispatch(setHotelPage(data.packageOffer));
