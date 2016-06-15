3add93fbfbc39227b0de29dc113736974f0b0dee
Jack Carlisle <jackcarlisle@hotmail.co.uk>
2016-06-15 12:27:16 +0100

    removes placeholder images and replaces them

--- src/constants/heroImages.js
+++ src/constants/heroImages.js
@@ -1,14 +1,14 @@
 const heroImages = [
   'https://cloud.githubusercontent.com/assets/12450298/14560385/62833d9a-0307-11e6-8a23-a0d1793d4e72.jpg',
-  'http://www.treasurebeachhotel.com/images/property_assets/treasure/page-bg.jpg',
-  'http://webneel.com/wallpaper/sites/default/files/images/04-2013/10-beach-sea-photography.jpg',
-  'http://webneel.com/wallpaper/sites/default/files/images/08-2013/10-star-fish-sea-beach-sand-wallpaper.jpg',
-  'http://fullhdpictures.com/wp-content/uploads/2015/03/Nice-Beach-Wallpapers.jpg',
-  'http://webneel.com/wallpaper/sites/default/files/images/04-2013/5-beach-sea-photography.jpg',
-  'https://static.pexels.com/photos/6769/sunset-beach-clouds-ocean.jpg',
-  'http://www.wallpapersdb.org/wallpapers/beach/lots_of_fishes_1920x1200.jpg',
-  'http://static.independent.co.uk/s3fs-public/thumbnails/image/2016/01/28/20/web-london-city-corbis.jpg',
-  'http://www.desktop-image.com/wp-content/uploads/2014/06/night-city-wallpaper2.jpg'
+  'http://www.tcdl.io.s3-website-eu-west-1.amazonaws.com/isearch/images/isearch_front_1.jpg',
+  'http://www.tcdl.io.s3-website-eu-west-1.amazonaws.com/isearch/images/isearch_front_3.jpg',
+  'http://www.tcdl.io.s3-website-eu-west-1.amazonaws.com/isearch/images/isearch_front_4.jpg',
+  'http://www.tcdl.io.s3-website-eu-west-1.amazonaws.com/isearch/images/isearch_front_5.jpg',
+  'http://www.tcdl.io.s3-website-eu-west-1.amazonaws.com/isearch/images/isearch_front_7.jpg',
+  'http://www.tcdl.io.s3-website-eu-west-1.amazonaws.com/isearch/images/isearch_front_8.jpg',
+  'http://www.tcdl.io.s3-website-eu-west-1.amazonaws.com/isearch/images/isearch_front_9.jpg',
+  'http://www.tcdl.io.s3-website-eu-west-1.amazonaws.com/isearch/images/isearch_front_10.jpg',
+  'https://cloud.githubusercontent.com/assets/12450298/14560385/62833d9a-0307-11e6-8a23-a0d1793d4e72.jpg'
 ];
 
 export default heroImages;
