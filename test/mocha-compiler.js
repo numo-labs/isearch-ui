9373e50e73582a74f0385b059eac2e785345fa04
Leonard Martin <leonard.martin@gmail.com>
2016-07-14 09:58:17 +0100

    Remove redundant test mock and orchestration files
    These are no longer needed when testing with karma, so should be removed.

--- test/mocha-compiler.js
+++
@@ -1,9 +0,0 @@
-function noop () {
-  return null;
-}
-require.extensions['.css'] = noop;
-// require.extensions['.png'] = noop;
-// require.extensions['.jpg'] = noop;
-// require.extensions['.jpeg'] = noop;
-// require.extensions['.gif'] = noop;
-require.extensions['.svg'] = noop;
