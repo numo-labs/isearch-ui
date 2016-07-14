c87099adfa25935c1b887e6e5fab98235b7133e1
Leonard Martin <leonard.martin@gmail.com>
2016-07-14 11:56:12 +0100

    Make karma runner the primary unit test runner
    Includes optimisations for webpack compiling to prevent memory limits being hit. These incude ignoring not critical assets, and compiling all test files into a single bundle.

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -1,24 +1,16 @@
-const browser = process.env.KARMA_BROWSER || 'PhantomJS';
-
 module.exports = function (config) {
   config.set({
 
     singleRun: true,
 
-    browsers: [browser],
+    browsers: ['PhantomJS'],
 
     frameworks: ['mocha', 'sinon'],
 
     files: [
       require.resolve('babel-polyfill/dist/polyfill.js'),
-      '../lib/**/*.test.js',
-      '../test/**/*.test.js'
+      './index.js'
     ],
-
-    phantomjsLauncher: {
-      // Have phantomjs exit if a ResourceError is encountered (useful if karma exits without killing phantom)
-      exitOnResourceError: true
-    },
 
     reporters: ['mocha'],
 
@@ -50,17 +42,8 @@ module.exports = function (config) {
             loader: 'json-loader'
           },
           {
-            test: /\.css$/,
-            loader: 'style-loader!css-loader'
-          },
-          {
-            test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
-            loader: 'file-loader?name=fonts/[name].[ext]'
-          },
-          {
-            test: /\.png$/,
-            loader: 'url-loader',
-            query: { mimetype: 'image/png' }
+            test: /\.(css|png|otf|svg)$/,
+            loader: 'ignore-loader'
           }
         ]
       },
