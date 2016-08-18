8dc944600ebcfac8722699d0b15b53079cadca40
Jack Carlisle <jackcarlisle@hotmail.co.uk>
2016-08-18 15:21:11 +0100

    fixes addArticleTag function #526

--- src/containers/destination.js
+++ src/containers/destination.js
@@ -2,9 +2,9 @@ import { connect } from 'react-redux';
 import DestinationFullPage from '../components/destination/';
 
 import * as ArticleActions from '../actions/article';
-import { addSingleTag } from '../actions/tags';
+import * as TagActions from '../actions/tags';
 import { routerActions } from 'react-router-redux';
-const Actions = {...ArticleActions, ...routerActions, addSingleTag};
+const Actions = {...ArticleActions, ...routerActions, ...TagActions};
 
 function mapStateToProps (state) {
   const { article: { articleContent, viewedArticles } } = state;
