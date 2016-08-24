576eef62ad85f2a15ee8215749a79ee2de3483ff
Jack Carlisle <jackcarlisle@hotmail.co.uk>
2016-08-24 16:49:59 +0100

    adds tests for the article page

--- test/components/article.test.js
+++ test/components/article.test.js
@@ -34,6 +34,15 @@ describe('Component', function () {
       expect(children).to.have.length(2);
       done();
     });
+    it('should render the correct children', function (done) {
+      const wrapper = shallow(<ArticleFullPage {...defaultProps} />);
+      expect(wrapper.find('div')).to.have.length(10);
+      expect(wrapper.find('NavHeader')).to.have.length(1);
+      expect(wrapper.find('section')).to.have.length(3);
+      expect(wrapper.find('video')).to.have.length(1);
+      expect(wrapper.find('ArticleFooter')).to.have.length(1);
+      done();
+    });
     it('should render a empty section if there is no article name', function (done) {
       global.dataLayer = null;
       const props = {...defaultProps, articleContent: {}};
