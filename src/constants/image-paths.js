0c38c70be286cf648891030e521779e14641d920
Leonard Martin <leonard.martin@gmail.com>
2016-08-24 15:51:50 +0100

    Consolidate image path constants
    Having one file per image doesn't scale very well at all. Instead have a single constants file which defines the loaction of images within a common root location.

---
+++ src/constants/image-paths.js
@@ -0,0 +1,5 @@
+const imagePath = 'https://www.tcdl.io.s3-website-eu-west-1.amazonaws.com/isearch/images';
+
+export const defaultImage = `${imagePath}/thomascook_fallback-image.jpg`;
+export const ratingIcon = `${imagePath}/rating.png`;
+export const weatherIcon = `${imagePath}/weather.png`;
