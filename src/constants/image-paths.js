79d8498939f3f462247b8355bcf3910f18d21e56
Leonard Martin <leonard.martin@gmail.com>
2016-08-24 16:50:12 +0100

    Add sun icon

--- src/constants/image-paths.js
+++ src/constants/image-paths.js
@@ -1,5 +1,5 @@
-const imagePath = 'https://www.tcdl.io.s3-website-eu-west-1.amazonaws.com/isearch/images';
+const imagePath = 'http://www.tcdl.io.s3-website-eu-west-1.amazonaws.com/isearch/images';
 
 export const defaultImage = `${imagePath}/thomascook_fallback-image.jpg`;
 export const ratingIcon = `${imagePath}/rating.png`;
-export const weatherIcon = `${imagePath}/weather.png`;
+export const weatherIcon = `${imagePath}/sun.svg`;
